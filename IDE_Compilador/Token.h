27f47905690e94429f35dc81a4c12d04787a39a9
liviazimermann <livia@asseinfo.com.br>
2026-05-20 10:31:14 -0300

    gals: adiciona acoes do gerador de codigo T4

--- IDE_Compilador/Token.h
+++ IDE_Compilador/Token.h
@@ -5,21 +5,19 @@
 
 #include <string>
 
-using namespace std;
-
 class Token
 {
 public:
-    Token(TokenId id, const string &lexeme, int position)
+    Token(TokenId id, const std::string &lexeme, int position)
       : id(id), lexeme(lexeme), position(position) { }
 
     TokenId getId() const { return id; }
-    const string &getLexeme() const { return lexeme; }
+    const std::string &getLexeme() const { return lexeme; }
     int getPosition() const { return position; }
 
 private:
     TokenId id;
-    string lexeme;
+    std::string lexeme;
     int position;
 };
 
